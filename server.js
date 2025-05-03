4ee626b04ff118d37bbeb0194d3c999841d01a79
ni <revecoFuentesd@gmail.com>
2025-05-03 12:49:30 -0400

    Actualizando conexion con la base de datos en railway

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const mysql = require("mysql2");
 const jwt = require("jsonwebtoken");
@@ -12,11 +13,13 @@ const SECRET_KEY = process.env.SECRET_KEY || "grupo_3";
 app.use(express.json());
 app.use(express.static("public"));
 
+
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "DanielDennisse_123",//DanielDennisse_123
-    database: "jardin_db"
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 connection.connect(err => {
